651ea42b00ce0e99002c7c3a805d0d489dcde1bb
Jhon Ace Sasutona <jhonacesasutona718@gmail.com>
2025-06-25 13:50:36 +0000

    Merged PR 122: Create Navbar
    feat: create navbar
    
    ![image.png](https://dev.azure.com/Darryle-Environment/08abce5a-dea4-41cf-8a20-2c3692fbda0d/_apis/git/repositories/21e6a09e-e6e8-43e9-8943-dd8157c280a3/pullRequests/122/attachments/image.png)
    
    Related work items: #87

--- client/src/layout/Navbar.jsx
+++ client/src/layout/Navbar.jsx
@@ -1,8 +1,69 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Button, Stack, Typography } from '@mui/material'
+
+
+import navItems from './Wrapper/nav-items/navItems'
+import Logo from 'assets/images/logo/logo.png'
 
 const Navbar = () => {
+  const navigate = useNavigate()
+ 
+
   return (
-    <div>Navbar</div>
+    <React.Fragment>
+      <Stack
+        sx={{
+          position: 'sticky',
+          top: 1,
+          width: '100%',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          paddingInline: 20,
+          alignItems: 'center',
+          zIndex: 10,
+          background: 'linear-gradient(to right, rgb(31, 90, 158), rgb(46, 119, 180))'
+        }}
+      >
+        <Stack
+          onClick={() => navigate('/')}
+          sx={{ cursor: 'pointer' }}
+        >
+          <img src={Logo} alt="Logo" style={{ height: '70px' }} />
+        </Stack>
+
+        {navItems.map((nav) => (
+
+          <React.Fragment key={nav._id}>
+            <Stack
+              direction="row"
+              alignItems="center"
+              spacing={1}
+              onClick={() => navigate(nav.link)}
+              sx={{ paddingBlock: '3%', cursor: 'pointer' }}
+            >
+              
+              {React.createElement(nav.icon, { style: { color: 'white', fontSize: 18 } })}
+              <Typography
+                variant="body2"
+                color="#fff"
+                sx={{ fontWeight: 'normal' }}
+              >
+                {nav.name}
+              </Typography>
+            </Stack>
+          </React.Fragment>
+        ))}
+
+        <Button
+          variant="contained"
+          sx={{ borderRadius: 2 }}
+          onClick={() => navigate('/login')}
+        >
+          Login
+        </Button>
+      </Stack>
+    </React.Fragment>
   )
 }
 
---
+++ client/src/layout/Wrapper/nav-items/navItems.js
@@ -0,0 +1,12 @@
+import { BookFilled, InfoCircleFilled, PhoneFilled, PictureFilled, SafetyCertificateFilled } from "@ant-design/icons";
+
+const navItems = [
+
+  { _id: 1, name: 'Book Now', link: '/book-now', icon: BookFilled  },
+  { _id: 2, name: 'About Us', link: '/about-us', icon: InfoCircleFilled },
+  { _id: 3, name: 'Admission', link: '/admission', icon: SafetyCertificateFilled  },
+  { _id: 4, name: 'Media', link: '/media', icon: PictureFilled  },
+  { _id: 5, name: 'Contact Us', link: '/contact-us', icon: PhoneFilled  }
+]
+
+export default navItems
