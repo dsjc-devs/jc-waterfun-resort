3411c47fec9438bf02d4f7f10a9b09dd393aa3d3
Christian <sarabiachristianlen21@gmail.com>
2025-06-25 22:32:28 +0800

    fix: logo dimension

--- client/src/components/logo/LogoMain.jsx
+++ client/src/components/logo/LogoMain.jsx
@@ -3,7 +3,7 @@ import logo from 'src/assets/images/logo/logo.png';
 // ==============================|| LOGO SVG ||============================== //
 
 const Logo = () => (
-  <img src={logo} alt="Logo" style={{ height: '90px', width: '100%', objectFit: 'cover', paddingBlock: 20 }} />
+  <img src={logo} alt="Logo" style={{ height: '130px', width: '100%', objectFit: 'cover', paddingBlock: 10 }} />
 )
 
 export default Logo;
