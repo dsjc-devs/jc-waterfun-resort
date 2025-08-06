82abeb7228e89ee115b2d47fa258a6ec19e59ae4
spgiron26 <spgiron26@gmail.com>
2025-08-06 14:26:38 +0000

    Merged PR 152: Contact Us API
    Contact us API endpoints
    
    Create new contact message
    ![image.png](https://dev.azure.com/Darryle-Environment/08abce5a-dea4-41cf-8a20-2c3692fbda0d/_apis/git/repositories/21e6a09e-e6e8-43e9-8943-dd8157c280a3/pullRequests/152/attachments/image.png)
    
    Get All contact (above: no phone number, below: with phone number)
    ![image (2).png](https://dev.azure.com/Darryle-Environment/08abce5a-dea4-41cf-8a20-2c3692fbda0d/_apis/git/repositories/21e6a09e-e6e8-43e9-8943-dd8157c280a3/pullRequests/152/attachments/image%20%282%29.png)
    
    Get by id
    ![image (3).png](https://dev.azure.com/Darryle-Environment/08abce5a-dea4-41cf-8a20-2c3692fbda0d/_apis/git/repositories/21e6a09e-e6e8-43e9-8943-dd8157c280a3/pullRequests/152/attachments/image%20%283%29.png)
    
    Related work items: #177

---
+++ server/controllers/contactUsControllers.js
@@ -0,0 +1,39 @@
+import expressAsync from "express-async-handler";
+import contactUsServices from "../services/contactUsServices.js";
+
+const createContact = expressAsync(async (req, res) => {
+  try {
+    const response = await contactUsServices.createContact(req.body);
+    res.status(201).json(response);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+const getAllContacts = expressAsync(async (req, res) => {
+  try {
+    const response = await contactUsServices.getAllContacts(req.query);
+    res.json(response);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+const getSingleContactById = expressAsync(async (req, res) => {
+  try {
+    const response = await contactUsServices.getSingleContactById(req.params.contactId);
+    res.json(response);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
+export {
+    createContact,
+    getAllContacts,
+    getSingleContactById,
+}
