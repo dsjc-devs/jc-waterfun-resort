019291e3ef2ab1eb6d3c82e8d81ee7c001744a34
Darryle Bacay <dmilesbacay@gmail.com>
2025-08-22 17:17:19 +0800

    refactor: make the title head dynamic

--- client/src/components/PageTitle.jsx
+++ client/src/components/PageTitle.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { APP_DEFAULT_NAME } from 'config/config';
+import { useGetResortDetails } from 'api/resort-details';
 import { Helmet } from 'react-helmet';
 
 const PageTitle = ({ title, isOnportal = true }) => {
+
+  const { resortDetails } = useGetResortDetails()
+
   return (
     <React.Fragment>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{`${title} - ${isOnportal ? `${APP_DEFAULT_NAME} Portal` : `${APP_DEFAULT_NAME}`}`}</title>
+        <title>{`${title} - ${isOnportal ? `${resortDetails?.companyInfo?.name} Portal` : `${resortDetails?.companyInfo?.name}`}`}</title>
         <link rel="canonical" href="http://mysite.com/example" />
       </Helmet>
     </React.Fragment>
