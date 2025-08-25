9092d3693f1bfbba9445a607dd1b34b26af6cfc4
Darryle Bacay <dmilesbacay@gmail.com>
2025-08-25 22:49:48 +0800

    feat: create fetcher for resort rates

---
+++ client/src/api/resortRates.js
@@ -0,0 +1,20 @@
+import { useMemo } from "react";
+import useSWR from "swr";
+import { fetcher } from "utils/axios";
+
+const endpoints = {
+  key: `${import.meta.env.VITE_API_KEY_}/${import.meta.env.VITE_API_VER}/resort-rates`,
+};
+
+export const useGetResortRates = () => {
+  const { data, isLoading, error, mutate } = useSWR(`/${endpoints.key}`, fetcher, options);
+
+  const memoizedValue = useMemo(() => ({
+    resortRates: data,
+    isLoading,
+    mutate,
+    error
+  }), [data, isLoading, mutate, error]);
+
+  return memoizedValue;
+};
