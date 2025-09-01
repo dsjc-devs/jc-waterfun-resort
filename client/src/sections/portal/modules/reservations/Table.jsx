5ec4fc16b691b34b41f767855d3e889d32aea6bb
Darryle Bacay <dmilesbacay@gmail.com>
2025-09-02 05:43:21 +0800

    feat: create reservations table

---
+++ client/src/sections/portal/modules/reservations/Table.jsx
@@ -0,0 +1,263 @@
+import React, { useMemo, useState } from 'react'
+import { useGetReservations } from 'api/reservations';
+import { Box, Button, Chip, Divider, Fade, Menu, MenuItem, Stack, Tooltip, Typography } from '@mui/material';
+import { EditOutlined, EllipsisOutlined, EyeOutlined, PlusOutlined, UserOutlined } from '@ant-design/icons';
+import { truncate } from "inspreact";
+
+import ReusableTable from 'components/ReusableTable'
+import Avatar from 'components/@extended/Avatar';
+import formatPeso from 'utils/formatPrice';
+import titleCase from 'utils/titleCaseFormatter';
+import useAuth from 'hooks/useAuth';
+import ConvertDate from 'components/ConvertDate';
+import IconButton from 'components/@extended/IconButton';
+import useGetPosition from 'hooks/useGetPosition';
+import AnimateButton from 'components/@extended/AnimateButton';
+import textFormatter from 'utils/textFormatter';
+
+const ReservationsTable = () => {
+  const { user } = useAuth()
+  const { isCustomer, isAdmin, isMasterAdmin } = useGetPosition()
+
+  const { data = {}, isLoading } = useGetReservations(isCustomer ? { userId: user?.userId } : {})
+  const { reservations = [] } = data || {}
+
+
+  const [openMenu, setOpenMenu] = useState({ anchorEl: null })
+
+  const handleMenuClick = (event) => {
+    setOpenMenu({ anchorEl: event.currentTarget })
+  }
+
+  const handleMenuClose = () => {
+    setOpenMenu({ anchorEl: null })
+  }
+
+  const columns = useMemo(() => {
+    const reservationColumn = {
+      id: 'reservationId',
+      align: 'left',
+      disablePadding: true,
+      label: 'Reservation',
+      renderCell: (row) => (
+        <Stack direction='row' alignItems='center' spacing={2}>
+          <Box>
+            <Avatar
+              variant='rectangle'
+              size='lg'
+              src={row?.accommodationData?.thumbnail}
+            />
+          </Box>
+          <Box>
+            <Typography
+              variant='subtitle2'
+              sx={{
+                color: 'primary.main',
+                '&:hover': { textDecoration: 'underline' }
+              }}
+            >
+              ID: #{row?.reservationId}
+            </Typography>
+            <Typography variant='subtitle2'>
+              {row?.accommodationData?.name}
+            </Typography>
+            <Typography variant='subtitle2' color='success.main'>
+              {formatPeso(row?.amount?.accommodationTotal)}
+            </Typography>
+            <Chip
+              size='small'
+              label={titleCase(row?.status)}
+              color={{
+                PENDING: 'warning',
+                CONFIRMED: 'primary',
+                COMPLETED: 'success',
+                RESCHEDULED: 'info',
+                ARCHIVED: 'error'
+              }[row?.status] || 'default'}
+            />
+          </Box>
+        </Stack >
+      )
+    };
+
+    const reservationDatesColumn = {
+      id: 'reservationDates',
+      label: 'Reservation Dates',
+      align: 'center',
+      renderCell: (row) => (
+        <Stack spacing={1} alignItems="center">
+          <Typography variant='subtitle2'>
+            Start Date: <ConvertDate dateString={row?.startDate} time />
+          </Typography>
+          <Typography variant='subtitle2'>
+            End Date: <ConvertDate dateString={row?.endDate} time />
+          </Typography>
+        </Stack>
+      )
+    };
+
+    const financialsColumn = {
+      id: 'financials',
+      label: isCustomer ? 'Payment' : 'Financials',
+      align: 'center',
+      renderCell: (row) => (
+        <Stack textAlign='left'>
+          <Chip
+            size='small'
+            label={textFormatter.fromSlug(titleCase(row?.paymentStatus))}
+            color={{
+              FULLY_PAID: 'success',
+              PARTIALLY_PAID: 'primary',
+              UNPAID: 'error'
+            }[row?.paymentStatus] || 'default'}
+          />
+          <Typography variant="body2">Accommodation Total: {formatPeso(row?.amount?.accommodationTotal)}</Typography>
+          <Typography variant="body2">Entrance Total: {formatPeso(row?.amount?.entranceTotal)}</Typography>
+          <Typography variant="body2">Minimum Payable: {formatPeso(row?.amount?.minimumPayable)}</Typography>
+          <Typography variant="body2">Total: {formatPeso(row?.amount?.total)}</Typography>
+          <Divider sx={{ mb: 1 }} />
+          <Typography variant="body2" color="success.dark">Paid: {formatPeso(row?.amount?.totalPaid)}</Typography>
+          <Divider sx={{ mb: 1 }} />
+          <Typography variant="body2" color="error">
+            Balance: {formatPeso(row?.amount?.total - row?.amount?.totalPaid)}
+          </Typography>
+        </Stack>
+      )
+    };
+
+    const customerColumn = {
+      id: 'userData',
+      align: 'left',
+      disablePadding: true,
+      label: 'Customer',
+      renderCell: (row) => (
+        <Stack flexDirection="row" gap={2} alignItems="center">
+          <Box>
+            <Avatar size="lg" src={row?.userData?.avatar} />
+          </Box>
+          <Box>
+            <Tooltip title={`User ID: ${row?.userData?.userId}`}>
+              <Typography variant="subtitle2" color="secondary.600">
+                #{truncate(row?.userData?.userId, 20)}
+              </Typography>
+            </Tooltip>
+            <Typography variant="subtitle1">
+              {row?.userData?.firstName} {row?.userData?.lastName}
+            </Typography>
+            <Typography variant="subtitle2" color="primary">
+              {row?.userData?.emailAddress}
+            </Typography>
+          </Box>
+        </Stack>
+      )
+    };
+
+    const guestsColumns = {
+      id: 'guests',
+      label: 'Guests',
+      align: 'center',
+      renderCell: (row) => {
+        const totalGuests = Object.values(row?.quantities || {}).reduce(
+          (sum, acc) => sum + (acc || 0),
+          0
+        );
+
+        return (
+          <Typography variant="body2">
+            <UserOutlined /> {totalGuests} Guests
+          </Typography>
+        );
+      }
+    };
+
+    const actionsColumn = {
+      id: 'actions',
+      align: 'center',
+      disablePadding: false,
+      label: '',
+      renderCell: (row) => {
+
+        return (
+          <IconButton
+            onClick={(e) => handleMenuClick(e)}
+          >
+            <EllipsisOutlined />
+          </IconButton>
+        )
+      }
+    }
+
+    if (isCustomer) {
+      return [reservationColumn, reservationDatesColumn, guestsColumns, financialsColumn, actionsColumn];
+    } else {
+      return [reservationColumn, customerColumn, reservationDatesColumn, guestsColumns, financialsColumn, actionsColumn];
+    }
+  }, [isCustomer]);
+
+  return (
+    <React.Fragment>
+      <ReusableTable
+        searchableColumns={[
+          "reservationId",
+          "userData.firstName",
+          "userData.lastName",
+          "userData.emailAddress",
+          "accommodationData.name",
+          "status",
+          "paymentStatus"
+        ]}
+        itemsPerPage={5}
+        columns={columns}
+        rows={reservations}
+        isLoading={isLoading}
+        noMessage="No reservations found."
+        settings={{
+          otherActionButton: (
+            <React.Fragment>
+              {(isAdmin || isMasterAdmin) && (
+                <AnimateButton>
+                  <Button
+                    variant='contained'
+                    startIcon={<PlusOutlined />}
+                  >
+                    Manually Reserve
+                  </Button>
+                </AnimateButton>
+              )}
+            </React.Fragment>
+          )
+        }}
+      />
+
+      <Menu
+        anchorEl={openMenu.anchorEl}
+        open={Boolean(openMenu.anchorEl)}
+        onClose={handleMenuClose}
+        TransitionComponent={Fade}
+      >
+        <MenuItem
+          onClick={() => {
+            handleMenuClose()
+          }}
+        >
+          <EyeOutlined style={{ marginRight: 8 }} />
+          View
+        </MenuItem>
+
+        {(isAdmin || isMasterAdmin) && (
+          <MenuItem
+            onClick={() => {
+              handleMenuClose()
+            }}
+          >
+            <EditOutlined style={{ marginRight: 8 }} />
+            Edit
+          </MenuItem>
+        )}
+      </Menu>
+
+    </React.Fragment>
+  )
+}
+
+export default ReservationsTable
