b36f4bd2cf260f58d5b1b57babae2d5f79035056
Darryle Bacay <dmilesbacay@gmail.com>
2025-09-06 20:54:23 +0800

    feat: create send email util

---
+++ server/utils/sendNodeMail.js
@@ -0,0 +1,35 @@
+import nodemailer from 'nodemailer';
+
+async function sendEmail(email, subject, content, attachments = [], bcc = []) {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.MAIL_CONFIGS_EMAIL,
+      pass: process.env.MAIL_CONFIGS_PASSWORD
+    }
+  });
+
+  let from = `123partners <${process.env.MAIL_CONFIGS_EMAIL}>`;
+
+  const mailConfigs = {
+    from,
+    to: email,
+    subject,
+    html: content,
+    attachments,
+    bcc
+  };
+
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mailConfigs, (error, info) => {
+      if (error) {
+        console.log(error);
+        return reject({ message: 'An error encountered.' });
+      }
+
+      return resolve({ message: 'Email has been sent.', info });
+    });
+  });
+}
+
+export default sendEmail;
