340884e62d87a30972d07c298755ce6ca136bcbb
Darryle Bacay <dmilesbacay@gmail.com>
2025-09-14 23:25:00 +0800

    refactor: put carousel in sections

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,74 +1,17 @@
-import { Box, Typography } from '@mui/material'
-
 import React from 'react'
-import Carousel from 'react-material-ui-carousel'
+import AboutUs from 'sections/landing-pages/AboutUs'
+import CarouselSection from 'sections/landing-pages/CarouselSection'
+import ContactUsHomePage from 'sections/landing-pages/ContactUs'
+import DiscoverSection from 'sections/landing-pages/DiscoverSection'
 import RateSection from 'sections/landing-pages/RateSection'
 
-import banner from 'assets/images/upload/banner.jpg'
-import banner2 from 'assets/images/upload/banner2.jpg'
-import banner3 from 'assets/images/upload/banner3.jpg'
-import banner4 from 'assets/images/upload/accom1.jpg'
-import banner5 from 'assets/images/upload/accom2.jpg'
-import banner6 from 'assets/images/upload/accom3.jpg'
-import banner7 from 'assets/images/upload/accom4.jpg'
-import banner8 from 'assets/images/upload/banner4.jpg'
-
-import AboutUs from 'sections/landing-pages/AboutUs'
-import ContactUs from 'sections/landing-pages/ContactUs'
-import DiscoverSection from 'sections/landing-pages/DiscoverSection'
-
 const Home = () => {
-  const items = [
-    banner,
-    banner2,
-    banner3,
-    banner4,
-    banner5,
-    banner6,
-    banner7,
-    banner8
-  ]
 
   return (
     <React.Fragment>
-      <Carousel
-        navButtonsAlwaysVisible
-        stopAutoPlayOnHover={false}
-        indicators={false}
-        height="100dvh"
-        duration={1000}
-        interval={5000}
-      >
-        {
-          items.map((item) => (
-            <Box sx={{ position: "relative", height: "100dvh" }}>
-              <Box
-                component="img"
-                src={item}
-                sx={{
-                  width: "100%",
-                  height: "100dvh",
-                  objectFit: "cover",
-                }}
-              />
-              <Box
-                sx={{
-                  position: "absolute",
-                  top: 0,
-                  left: 0,
-                  width: "100%",
-                  height: "100%",
-                  backgroundColor: "rgba(0, 0, 0, 0.2)",
-                }}
-              />
-            </Box>
-
-          ))
-        }
-      </Carousel>
-
+      <CarouselSection />
       <RateSection />
-      <ContactUs />
+      <ContactUsHomePage />
       <AboutUs />
       <DiscoverSection />
     </React.Fragment>
---
+++ client/src/sections/landing-pages/CarouselSection.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import Carousel from 'react-material-ui-carousel'
+import banner from 'assets/images/upload/banner.jpg'
+import banner2 from 'assets/images/upload/banner2.jpg'
+import banner3 from 'assets/images/upload/banner3.jpg'
+import banner4 from 'assets/images/upload/accom1.jpg'
+import banner5 from 'assets/images/upload/accom2.jpg'
+import banner6 from 'assets/images/upload/accom3.jpg'
+import banner7 from 'assets/images/upload/accom4.jpg'
+import banner8 from 'assets/images/upload/banner4.jpg'
+import { Box } from '@mui/material'
+
+const CarouselSection = () => {
+  const items = [
+    banner,
+    banner2,
+    banner3,
+    banner4,
+    banner5,
+    banner6,
+    banner7,
+    banner8
+  ]
+
+  return (
+    <Carousel
+      navButtonsAlwaysVisible
+      stopAutoPlayOnHover={false}
+      indicators={false}
+      height="100dvh"
+      duration={1000}
+      interval={5000}
+    >
+      {
+        items.map((item) => (
+          <Box sx={{ position: "relative", height: "100dvh" }}>
+            <Box
+              component="img"
+              src={item}
+              sx={{
+                width: "100%",
+                height: "100dvh",
+                objectFit: "cover",
+              }}
+            />
+            <Box
+              sx={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                backgroundColor: "rgba(0, 0, 0, 0.2)",
+              }}
+            />
+          </Box>
+
+        ))
+      }
+    </Carousel>
+  )
+}
+
+export default CarouselSection
